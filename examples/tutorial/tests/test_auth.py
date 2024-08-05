e18ce6c4b0483193b19629b39d570926792fde22
samir-bueno <samirbueno585@gmail.com>
2024-08-05 11:15:41 -0300

    agregue un test para chequear que se guarde correctamente el hash de la contraseña

--- examples/tutorial/tests/test_auth.py
+++ examples/tutorial/tests/test_auth.py
@@ -3,22 +3,23 @@ from flask import g
 from flask import session
 
 from flaskr.db import get_db
-
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
 
 def test_register(client, app):
     # test that viewing the page renders without template errors
     assert client.get("/auth/register").status_code == 200
 
     # test that successful registration redirects to the login page
-    response = client.post("/auth/register", data={"username": "a", "password": "a"})
-    assert response.headers["Location"] == "/auth/login"
+    response = client.post("/auth/register", data={"username": "a", "password": "b"})
+    assert "/auth/login" in response.headers["Location"]
 
     # test that the user was inserted into the database
-    with app.app_context():
-        assert (
-            get_db().execute("SELECT * FROM user WHERE username = 'a'").fetchone()
-            is not None
-        )
+    with app.app_context():      
+            usuario = get_db().execute("SELECT * FROM user WHERE username = 'a'").fetchone()
+            assert (usuario is not None)
+            assert(check_password_hash(usuario["password"], "b"))
+        
 
 
 @pytest.mark.parametrize(
